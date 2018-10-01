37c8357814383a35fba365e2d03e7b005658cddb
mxprshn <mxprshn@gmail.com>
2018-10-02 00:08:55 +0300

    It also makes array partition now

--- homework3/hw3.1/hw3.1.cpp
+++ homework3/hw3.1/hw3.1.cpp
@@ -3,28 +3,47 @@
 #include <algorithm>
 
 
-int insertionSort(int *sourceArray, int sourceArrayLen)
+int insertionSort(int *insSourceArray, int insSourceArrayLength)
 {
-	for (int i = 1; i < sourceArrayLen; ++i)
+	for (int i = 1; i < insSourceArrayLength; ++i)
 	{
-		const int key = sourceArray[i];
-	
-		for (int counter = i; counter >= 0; --counter)
+		const int key = insSourceArray[i];
+		int counter = i;
+
+		while ((counter >= 0) && (key < insSourceArray[counter - 1]))
 		{
-			if (key < sourceArray[counter - 1])
-			{
-				std::swap(sourceArray[counter - 1], sourceArray[counter]);
-			}
+				std::swap(insSourceArray[counter - 1], insSourceArray[counter]);
+				--counter;
 		}
 	}
 
 	return 0;
 }
 
+int arrayPartition(int *unpartedArray, int leftBorder, int rightBorder)
+{
+	const int pivot = unpartedArray[leftBorder];
+	int position = leftBorder + 1;
+
+	for (int i = leftBorder + 1; i < rightBorder; ++i)
+	{
+		if (unpartedArray[i] < pivot)
+		{
+			std::swap(unpartedArray[i], unpartedArray[position]);
+			++position;
+		}
+	}
+
+	std::swap(unpartedArray[leftBorder], unpartedArray[position - 1]);
+	return 0;
+}
+
+int quickSort(int *)
+
 int main()
 {
-	int testArray[]{ 2, 7, 1, 0, 45, -6, 10 };
-	insertionSort(testArray, 7);
+	int testArray[]{ 5, 7, 1, 0, 45, -6, 10 };
+	arrayPartition(testArray, 0, 7);
 	for (int i = 0; i < 7; ++i)
 	{
 		printf("%d ", testArray[i]);
