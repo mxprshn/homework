016d9dc6c1d29c65e46b9a901d316fe3b9f4eaa4
mxprshn <mxprshn@gmail.com>
2018-10-04 19:28:23 +0300

    Bugs fixed (it finally works) and test function added

--- homework3/hw3.1/hw3.1.cpp
+++ homework3/hw3.1/hw3.1.cpp
@@ -1,23 +1,23 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <algorithm>
+#include <stdlib.h>
+#include <time.h>
 
 
-int insertionSort(int *insSourceArray, int insLeftBorder, int insRightBorder)
+void insertionSort(int *insSourceArray, int insLeftBorder, int insRightBorder)
 {
-	for (int i = insLeftBorder; i < insRightBorder; ++i)
+	for (int i = insLeftBorder; i <= insRightBorder; ++i)
 	{
 		const int key = insSourceArray[i];
 		int counter = i;
 
-		while ((counter >= insLeftBorder) && (key < insSourceArray[counter - 1]))
+		while ((counter > insLeftBorder) && (key < insSourceArray[counter - 1]))
 		{
 				std::swap(insSourceArray[counter - 1], insSourceArray[counter]);
 				--counter;
 		}
 	}
-
-	return 0;
 }
 
 int arrayPartition(int *partitionArray, int partLeftBorder, int partRightBorder)
@@ -25,7 +25,7 @@ int arrayPartition(int *partitionArray, int partLeftBorder, int partRightBorder)
 	const int pivot = partitionArray[partLeftBorder];
 	int position = partLeftBorder + 1;
 
-	for (int i = partLeftBorder + 1; i < partRightBorder; ++i)
+	for (int i = partLeftBorder + 1; i <= partRightBorder; ++i)
 	{
 		if (partitionArray[i] < pivot)
 		{
@@ -35,7 +35,8 @@ int arrayPartition(int *partitionArray, int partLeftBorder, int partRightBorder)
 	}
 
 	std::swap(partitionArray[partLeftBorder], partitionArray[position - 1]);
-	return partitionArray[position - 1];
+		
+	return position - 1;
 }
 
 int quickSort(int *qsSourceArray, int sortLeftBorder, int sortRightBorder)
@@ -48,13 +49,54 @@ int quickSort(int *qsSourceArray, int sortLeftBorder, int sortRightBorder)
 
 	int divisor = arrayPartition(qsSourceArray, sortLeftBorder, sortRightBorder);
 	quickSort(qsSourceArray, sortLeftBorder, divisor - 1);
-	quickSort(qsSourceArray, divisor, sortRightBorder);
+	quickSort(qsSourceArray, divisor + 1, sortRightBorder);
 
 	return 0;
 } 
 
+bool programTest()
+{
+	srand(time(0));
+	const int testArrayLength = rand() % 30 + 10;
+	int *testArray1 = new int[testArrayLength]{0};
+	int *testArray2 = new int[testArrayLength]{0};
+
+	for (int i = 0; i < testArrayLength; ++i)
+	{
+		testArray1[i] = rand();
+		testArray2[i] = testArray1[i];
+	}
+
+	quickSort(testArray1, 0, testArrayLength - 1);
+	insertionSort(testArray2, 0, testArrayLength - 1);
+
+	for (int i = 1; i < testArrayLength; ++i)
+	{
+		if ((testArray1[i] != testArray2[i]) || (testArray1[i] < testArray1[i - 1]))
+		{
+			delete[] testArray1;
+			delete[] testArray2;
+			return false;
+		}
+	}
+
+	delete[] testArray1;
+	delete[] testArray2;
+	return true;
+}
+
 int main()
 {
+	if (programTest)
+	{
+		printf("Test completed.\n");
+	}
+	else
+	{
+		printf("Test not completed.\n");
+		return 0;
+	}
+
 	int arrayLength = 0;
 	printf("Enter the length of the array: ");
 	scanf("%d", &arrayLength);
@@ -67,8 +109,8 @@ int main()
 	{
 		scanf("%d", &sourceArray[i]);
 	}
-
-	quickSort(sourceArray, 0, arrayLength);
+	
+	quickSort(sourceArray, 0, arrayLength - 1);
 
 	for (int i = 0; i < arrayLength; ++i)
 	{
