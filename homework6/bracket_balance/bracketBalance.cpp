adc630370faae8960eebf92babef2b6c481ed78b
mxprshn <mxprshn@gmail.com>
2018-11-11 00:36:55 +0300

    Added pointer cleaning after stack deleting and added main module

--- homework6/bracket_balance/bracketBalance.cpp
+++ homework6/bracket_balance/bracketBalance.cpp
@@ -1,7 +1,6 @@
 #include "stack.h"
 #include "bracketBalance.h"
-#include "tests.h"
-#include <iostream>
+#include <string>
 
 bool checkBalance(std::string sourceString)
 {
@@ -35,37 +34,7 @@ bool checkBalance(std::string sourceString)
 		}
 	}
 
-	bool isBalanced = isEmpty(bracketStack);
+	const bool isBalanced = isEmpty(bracketStack);
 	deleteStack(bracketStack);
 	return isBalanced;
-}
-
-int main()
-{
-	if (!programTest())
-	{
-		std::cout << "Test not completed.\n";
-		return 1;
-	}
-	else
-	{
-		std::cout << "Test completed.\n";
-	}
-
-	std::cout << "Enter a string to check its bracket balance: ";
-
-	std::string sourceString{};
-
-	std::getline(std::cin, sourceString);
-
-	if (checkBalance(sourceString))
-	{
-		std::cout << "Brackets are balanced.\n";
-	}
-	else
-	{
-		std::cout << "Brackets are not balanced.\n";
-	}
-
-	return 0;
 }
--- homework6/bracket_balance/stack.h
+++ homework6/bracket_balance/stack.h
@@ -12,7 +12,7 @@ int pop(Stack *stack, bool &result);
 bool isEmpty(Stack *stack);
 
 //delete the stack completely
-void deleteStack(Stack *stack);
+void deleteStack(Stack *&stack);
 
 //create a new stack
 Stack *createStack();
