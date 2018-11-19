eca6b31489655880afd9a785cfc86bc78a407d94
mxprshn <mxprshn@gmail.com>
2018-11-19 21:15:41 +0300

    Added endls

--- homework7/tree_set/userInterface.cpp
+++ homework7/tree_set/userInterface.cpp
@@ -3,20 +3,20 @@
 
 void userInterface(Set *set)
 {
-	std::cout << "Set by mxprshn. All rights reserved. 2018 \n\n";
-	std::cout << "Command list:\n";
-	std::cout << "0 - Exit program\n";
-	std::cout << "1 - Add a new value to the set\n";
-	std::cout << "2 - Print elements of the set in ascending order\n";
-	std::cout << "3 - Print elements of the set in descending order\n";
-	std::cout << "4 - Check existence of value in the set\n";
-	std::cout << "5 - Remove value from the set\n";
+	std::cout << "Set by mxprshn. All rights reserved. 2018" << std::endl << std::endl;
+	std::cout << "Command list:" << std::endl;
+	std::cout << "0 - Exit program" << std::endl;
+	std::cout << "1 - Add a new value to the set" << std::endl;
+	std::cout << "2 - Print elements of the set in ascending order" << std::endl;
+	std::cout << "3 - Print elements of the set in descending order" << std::endl;
+	std::cout << "4 - Check existence of value in the set" << std::endl;
+	std::cout << "5 - Remove value from the set" << std::endl;
 
 	int commandCode = -1;
 
 	while (commandCode != 0)
 	{
-		std::cout << "\nEnter a command: ";
+		std::cout << std::endl << "Enter a command: ";
 		std::cin >> commandCode;
 
 		switch (commandCode)
@@ -29,7 +29,7 @@ void userInterface(Set *set)
 
 			if (!add(set, newValue))
 			{
-				std::cout << "This value already exists.\n";
+				std::cout << "This value already exists." << std::endl;
 			}
 
 			break;
@@ -41,7 +41,7 @@ void userInterface(Set *set)
 				std::cout << "The set is empty.";
 			}
 
-			std::cout << "\n";
+			std::cout << std::endl;
 			break;
 		}
 		case 3:
@@ -51,7 +51,7 @@ void userInterface(Set *set)
 				std::cout << "The set is empty.";
 			}
 
-			std::cout << "\n";
+			std::cout << std::endl;
 			break;
 		}
 		case 4:
@@ -62,11 +62,11 @@ void userInterface(Set *set)
 
 			if (exists(set, targetValue))
 			{
-				std::cout << "This value exists.\n";
+				std::cout << "This value exists." << std::endl;
 			}
 			else
 			{
-				std::cout << "This value doesn't exist.\n";
+				std::cout << "This value doesn't exist." << std::endl;
 			}
 
 			break;
@@ -79,7 +79,7 @@ void userInterface(Set *set)
 
 			if (!remove(set, targetValue))
 			{
-				std::cout << "This value doesn't exist.\n";
+				std::cout << "This value doesn't exist." << std::endl;
 			}
 
 			break;
