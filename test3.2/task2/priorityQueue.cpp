66f89c182c9c9bb3a79b06bce4596d00c4a65899
mxprshn <mxprshn@gmail.com>
2018-12-21 16:30:56 +0300

    Changed queue module

--- test3.2/task2/priorityQueue.cpp
+++ test3.2/task2/priorityQueue.cpp
@@ -1,86 +1,82 @@
 #include "priorityQueue.h"
-#include <vector>
-#include <algorithm>
+
+struct Node
+{
+	int key = 0;
+	int value = 0;
+	Node *next = nullptr;
+};
 
 struct PriorityQueue
 {
-	std::vector<std::pair<int, int>> elements;
+	Node *maximum = nullptr;
 };
-
-PriorityQueue *newQueue()
-{
-	return new PriorityQueue;
-}
-
-void upHeap(PriorityQueue *queue, size_t position)
-{
-	while ((position != 0) && (queue->elements[position].first >= queue->elements[(position - 1) / 2].first))
-	{
-		swap(queue->elements[position], queue->elements[(position - 1) / 2]);
-		position = (position - 1) / 2;
-	}
-}
-
-void downHeap(PriorityQueue *queue, size_t position)
-{
-	while ((position * 2 + 1) < queue->elements.size())
-	{
-		int maxChild = position * 2 + 1;
-		if ((position * 2 + 2 < queue->elements.size()) && (queue->elements[position * 2 + 2].first > queue->elements[position * 2 + 1].first))
-		{
-			maxChild = position * 2 + 2;
-		}
-
-		if (queue->elements[maxChild].first < queue->elements[position].first)
-		{
-			break;
-		}
-
-		swap(queue->elements[maxChild], queue->elements[position]);
-		position = maxChild;
-	}
-
-	if (position + 1 < queue->elements.size() && (queue->elements[position].first == queue->elements[position + 1].first))
-	{
-		swap(queue->elements[position], queue->elements[position + 1]);
-	}
-}
-
-void enqueue(PriorityQueue *queue, const int key, const int value)
-{
-	std::pair<int, int> newElement = {key, value};
-
-	//if (!isEmpty(queue) && key == queue->elements[0].first)
-	//{
-	//	queue->elements.insert(queue->elements.begin(), newElement);
-	//	return;
-	//}
-
-	queue->elements.push_back(newElement);
-	upHeap(queue, queue->elements.size() - 1);
-}
-
-int dequeue(PriorityQueue *queue)
-{
-	if (isEmpty(queue))
-	{
-		return -1;
-	}
-
-	const std::pair<int, int> maximum = queue->elements[0];
-	swap(queue->elements[0], queue->elements[queue->elements.size() - 1]);
-	queue->elements.pop_back();
-	downHeap(queue, 0);
-	return maximum.second;
-}
 
 bool isEmpty(PriorityQueue *queue)
 {
-	return queue->elements.size() == 0;
+	return (queue->maximum == nullptr);
+}
+
+void enqueue(PriorityQueue *queue, const int newKey, const int newValue)
+{
+	const auto newElement = new Node{ newKey, newValue, nullptr };
+
+	if (isEmpty(queue))
+	{
+		queue->maximum = newElement;
+	}
+	else
+	{
+		Node *leftNode = nullptr;
+		Node *rightNode = queue->maximum;
+
+		while (rightNode != nullptr && newElement->key < rightNode->key)
+		{
+			leftNode = rightNode;
+			rightNode = rightNode->next;
+		}
+
+		if (leftNode != nullptr)
+		{
+			newElement->next = rightNode;
+			leftNode->next = newElement;
+		}
+		else
+		{
+			newElement->next = queue->maximum;
+			queue->maximum = newElement;
+		}		
+	}	
 }
 
 void deleteQueue(PriorityQueue *&queue)
 {
+	while (!isEmpty(queue))
+	{
+		const Node *temp = queue->maximum;
+		queue->maximum = queue->maximum->next;
+		delete temp;
+	}
+
 	delete queue;
 	queue = nullptr;
+}
+
+int dequeue(PriorityQueue *queue)
+{
+	if (queue->maximum != nullptr)
+	{
+		const int maximumValue = queue->maximum->value;
+		Node *temp = queue->maximum;
+		queue->maximum = queue->maximum->next;
+		delete temp;
+		return maximumValue;
+	}
+
+	return -1;
+}
+
+PriorityQueue *newQueue()
+{
+	return new PriorityQueue;
 }
--- test3.2/task2/priorityQueue.h
+++ test3.2/task2/priorityQueue.h
@@ -1,19 +1,18 @@
 #pragma once
-#include <vector>
 
 struct PriorityQueue;
 
-//returns pointer to the new queue
-PriorityQueue *newQueue();
-
-//adds a new element to the queue
-void enqueue(PriorityQueue *queue, const int key, const int value);
-
-//returns the maximum key element in the queue and removes it
-int dequeue(PriorityQueue *queue);
-
-//checks the queue for emptiness
+//check the queue for emptiness
 bool isEmpty(PriorityQueue *queue);
 
-//deletes the queue completely
+//add a new element to the queue
+void enqueue(PriorityQueue *queue, const int newKey, const int newValue);
+
+//delete the queue completely
 void deleteQueue(PriorityQueue *&queue);
+
+//qet the minimum value from the queue and remove it
+int dequeue(PriorityQueue *queue);
+
+//create a new queue
+PriorityQueue *newQueue();
